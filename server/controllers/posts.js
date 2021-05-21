c608b592b167d61be7bbb721556fbc780f9dba5b
Tony Tanaka <tony.h.tanaka@gmail.com>
2021-05-21 15:27:49 +0200

    fixed 2 bugs

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,11 +1,10 @@
 import PostMessage from "../models/postMessage.js";
 import mongoose from "mongoose";
 
-export const getPosts = async (req,res) => {
+export const getPosts = async (req, res) => {
     try {
-        const postMessages = await PostMessage.find()
-        res.status(200).json(postMessages)
-        res.json(posts)
+        const postMessages = await PostMessage.find();
+        res.status(200).json(postMessages);
 }
     catch (error) {
         res.status(404).json({ message: error.message })
