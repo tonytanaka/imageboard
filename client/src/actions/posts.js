a76ceed8d98bdd9504b0bc966d6650a75ff47737
Tony Tanaka <tony.h.tanaka@gmail.com>
2021-05-21 10:44:00 +0200

    google sign-in

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,44 +1,45 @@
 import * as api from "../api";
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes";
 
-// Action creators
+//Action creators
 
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
-    dispatch({
-      type: "FETCH_ALL",
-      payload: data,
-    });
+    dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
 
 export const actionCreatePost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
-    dispatch({
-      type: "CREATE",
-      payload: data,
-    });
+    dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error.message);
   }
 };
 
-export const updatePost = (id, post) => async(dispatch) => {
+export const updatePost = (id, post) => async (dispatch) => {
   try {
-    const {data} = await api.updatePost(id, post);
-    dispatch({type: "UPDATE", payload: data})
-  } catch(error) {
-    console.log(error)
+    const { data } = await api.updatePost(id, post);
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
 export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id);
-    dispatch({ type: "DELETE", payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
@@ -47,7 +48,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
-    dispatch({ type: "LIKE", payload: data });
+    dispatch({ type: LIKE, payload: data });
   } catch (error) {
     console.log(error);
   }
