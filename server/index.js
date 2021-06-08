42bf6c6984c3f3511fefb759b212a7070610e101
Tony Tanaka <tony.h.tanaka@gmail.com>
2021-06-08 12:16:38 +0200

    some update

--- server/index.js
+++ server/index.js
@@ -9,19 +9,24 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", '*');
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//   next();
+// });
 
 
 //general setup
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+  {
+    origin : process.env.FRONTEND || 'http://localhost:3000',
+    credentials : true
+  }
+));
 
 
 
